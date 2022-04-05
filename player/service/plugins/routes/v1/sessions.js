7e8936188a0bf213727a00db54ea059a335fb91e
John Martin <john.martin.ctr@adlnet.gov>
2022-04-05 01:11:25 -0400

    v1 unit tests + code fixes.

--- player/service/plugins/routes/v1/sessions.js
+++ player/service/plugins/routes/v1/sessions.js
@@ -1,9 +1,12 @@
 /*
     Copyright 2021 Rustici Software
+
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
+
         http://www.apache.org/licenses/LICENSE-2.0
+
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,49 +16,55 @@
 "use strict";
 
 const Boom = require("@hapi/boom"),
-    Wreck = require("@hapi/wreck"),
-    Session = require("../lib/session");
+  Wreck = require("@hapi/wreck"),
+  Session = require("../lib/session");
+
+async function handleSession(req, h, args) {
+  var sessionId = req.params.id;
+  var tenantId = req.auth.credentials.tenantId;
+  var db = req.server.app.db;
+
+  if (args && args.doAbandon) {
+    var lrsWreck = Wreck.defaults(
+      await req.server.methods.lrsWreckDefaults(req)
+    );
+
+    await Session.abandon(sessionId, tenantId, "api", { db, lrsWreck });
+
+    return null;
+  }
+
+  var result = await Session.load(sessionId, tenantId, { db });
+
+  if (!result) {
+    return Boom.notFound();
+  }
+
+  return result;
+}
 
 module.exports = {
-    name: "catapult-player-api-routes-v1-sessions",
-    register: (server, options) => {
-        server.route(
-            [
-                {
-                    method: "GET",
-                    path: "/session/{id}",
-                    options: {
-                        tags: ["api"]
-                    },
-                    handler: async (req, h) => {
-                        const result = await Session.load(req.params.id, req.auth.credentials.tenantId, {db: req.server.app.db});
+  handleSession,
+  name: "catapult-player-api-routes-v1-sessions",
+  register: (server, options) => {
+    server.route([
+      {
+        method: "GET",
+        path: "/session/{id}",
+        options: {
+          tags: ["api"],
+        },
+        handler: handleSession(req, h),
+      },
 
-                        if (! result) {
-                            return Boom.notFound();
-                        }
-
-                        return result;
-                    }
-                },
-
-                {
-                    method: "POST",
-                    path: "/session/{id}/abandon",
-                    options: {
-                        tags: ["api"]
-                    },
-                    handler: async (req, h) => {
-                        const sessionId = req.params.id,
-                            tenantId = req.auth.credentials.tenantId,
-                            db = req.server.app.db,
-                            lrsWreck = Wreck.defaults(await req.server.methods.lrsWreckDefaults(req));
-
-                        await Session.abandon(sessionId, tenantId, "api", {db, lrsWreck});
-
-                        return null;
-                    }
-                }
-            ]
-        );
-    }
+      {
+        method: "POST",
+        path: "/session/{id}/abandon",
+        options: {
+          tags: ["api"],
+        },
+        handler: handleSession(req, h, { doAbandon: true }),
+      },
+    ]);
+  },
 };
---
+++ player/test/courses.js
@@ -0,0 +1,40 @@
+// Test file for courses.js
+"use strict";
+
+var expect = require("chai").expect;
+var sinon = require("sinon");
+var proxyquire = require("proxyquire");
+
+var courses = require("../service/plugins/routes/v1/courses");
+
+describe("buildPayload for courses.js", function() {
+  it("Checks that the payload comes back with byte size and file output by default", function() {
+    var payloadObj = courses.buildPayload();
+
+    expect(payloadObj).to.haveOwnProperty("maxBytes");
+  });
+
+  it("Checks that the payload returns a Joi object when validating", function() {
+    var args = { withValidate: true };
+
+    var payloadObj = courses.buildPayload(args);
+    var payloadLabel = payloadObj._flags.label;
+
+    expect(payloadLabel).to.equal("Request-LaunchUrl");
+  });
+});
+
+describe.skip("getOptions for courses.js", function() {
+  it("Checks that only tags are returned if no input is given", function() {
+    var options = courses.getOptions();
+
+    expect(options).to.eql({ tags: ["api"] });
+  });
+
+  it("Checks that auth info is returned with tags if requested", function() {
+    var args = { withAuth: true };
+    var options = courses.getOptions(args);
+
+    expect(options).to.eql({ auth: "basic", tags: ["api"] });
+  });
+});
