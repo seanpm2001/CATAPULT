fc0ec64b8700049f4799eb756086ea777b16aea4
Megan <megan.bohland@simisinc.com>
2022-04-08 15:16:25 -0400

    finished several more functions

--- player/test/session.spec.js
+++ player/test/session.spec.js
@@ -8,6 +8,8 @@ const Session = require('../service/plugins/routes/lib/session.js');//So Session
 const { assert } = require('chai');
 const spy = sinon.spy();
 const sandbox = require("sinon").createSandbox();
+chai.use(chaiAsPromised);
+chai.should();
 
 describe('Test of load function', function() {
 
@@ -79,7 +81,7 @@ describe('Test of getSession function', function() {
 	})
 }),
 
-describe.only('Test of loadForChange function', function() {
+describe('Test of loadForChange function', function() {
    
 	var lfcStub;
 	var txn = {}; //a transaction object
@@ -91,11 +93,8 @@ describe.only('Test of loadForChange function', function() {
 		regCourseAu,
 		registration,
 		courseAu
-
+	var regCourseAuObject = {courseAu:0 } //mock regCourseAu object assigned in this function
 	var txnReturn = [sessions= 2, registrationsCoursesAus =13, registrations = 26, coursesAus =4]
-
-	chai.use(chaiAsPromised);
-	chai.should();
 
 	beforeEach(() =>{
 		lfcStub = sinon.stub(Session,'loadForChange');
@@ -108,13 +107,18 @@ describe.only('Test of loadForChange function', function() {
 
 	it('calls the tryGetQueryResult function and waits for updated txn, assigning it to queryResult variable', async function()  {
 		
-		lfcStub.callsFake(() => Promise.resolve(txn));
+		tgqrStub = sinon.stub(Session,'tryGetQueryResult');
 
-		queryResult = await Session.loadForChange(txn, sessionId, tenantId);
+		lfcStub.callsFake(() => Promise.resolve(txn));
+		tgqrStub.callsFake(() => Promise.resolve(txn));
+
+		queryResult = await Session.tryGetQueryResult(txn, sessionId, tenantId);
 		
 		assert.equal(queryResult, txn)
 		expect(queryResult).to.equal(txn);
-	
+
+		tgqrStub.reset();
+		tgqrStub.restore();
 	}),
 
 	it('verifies queryResult was retrieved and uses it to assign session registration information', async function() {
@@ -123,21 +127,34 @@ describe.only('Test of loadForChange function', function() {
 		expect(Session.loadForChange(txn, sessionId, tenantId));
 
 		if (queryResult == true) {
+
 			txnRollback = false;
-			//does assignValues even need to be a function? I mean it's only used once.
-			console.log('is this being trigered>')
-				var [session,
+
+			var [session,
 					regCourseAu,
 					registration,
 					courseAu
-				 = txnReturn;
-				console.log(txnReturn, session)
-				console.log(sessions)
+				] = txnReturn;
+
+			regCourseAuObject.courseAu = courseAu;
 		}
 
 		expect(session).to.equal(2);
+		expect(regCourseAu).to.equal(13);
+		expect(registration).to.equal(26);
+		expect(courseAu).to.equal(4);
+		expect(regCourseAuObject.courseAu).to.equal(4);
 		expect(txnRollback).to.be.false;
-		expect(errorAndRollback).to.throw('session: ');
+	}),
+	
+	it('returns the successfully updated Session registration information', async function()  {
+		
+		lfcStub.withArgs(txn, sessionId, tenantId).callsFake(() => Promise.resolve(session, regCourseAu, registration, courseAu));
+
+		queryResult = await Session.loadForChange(txn, sessionId, tenantId);
+		
+		assert.equal(queryResult, session, regCourseAu, registration, courseAu)
+		expect(queryResult).to.equal(session, regCourseAu, registration, courseAu);
 	}),
 	
 	it('throws an error if queryResult was not retrieved successfully and rolls back transaction', async function() {
@@ -159,7 +176,7 @@ describe.only('Test of loadForChange function', function() {
 
 	
 }),
-/////////////////////////////
+
 describe('Test of tryGetQueryResult function', function() {
    
 	var tgqrStub;
@@ -244,4 +261,120 @@ describe('Test of getQueryResult', function() {
 		
 		expect(gqrStubcheckThrow).to.throw('Failed to select session, registration course AU, registration and course AU for update:');
 	});
+}),
+
+describe.only('Test of abandon function', function() {
+   
+	var abandonStub;
+	var db = {transaction: txn = {}}
+	var lrsWreck;
+	var dbWreckArg = [db, lrsWreck]; //stand in for object passed as arg to abandon
+	var txn = {}; //a transaction object
+	var txnRollback = false; //to track whether the mocked txn is rolled back
+	var sessionId = 1234; 
+	var tenantId = 'tenantID'; 
+	var by; //stand in for what exactly???
+	var queryResult;
+	var session, //Stand in for Session values that are returned
+		regCourseAu,
+		registration,
+		courseAu
+	//var regCourseAuObject = {courseAu:0 } //mock regCourseAu object assigned in this function
+	var sessionReturn = [rtnSession= 2, rtnRegCourseAu =13, returnRegistration = 26, rtnCoursesAu =4]
+
+	beforeEach(() =>{
+		abandonStub = sinon.stub(Session,'abandon');
+	});
+
+	afterEach(() =>{
+		abandonStub.reset();
+		abandonStub.restore();
+	});
+
+	it('retrieves the database transaction object (the txn) ', async function() {
+			
+		abandonStub.withArgs(db).callsFake(() => Promise.resolve(db.transaction));
+
+		queryResult = await Session.abandon(db);
+
+		expect(queryResult).to.eql(txn);//note use of 'eql' here as values are the same but reference point is not
+
+	})
+
+	it('tries to call the loadForChange function and waits for updated Session information', async function()  {
+		
+		lfcStub = sinon.stub(Session,'loadForChange');
+
+		lfcStub.withArgs(txn, sessionId, tenantId, courseAu).callsFake(() => Promise.resolve(sessionReturn));
+
+		queryResult = await Session.loadForChange(txn, sessionId, tenantId, courseAu);
+			
+		var [session,
+			regCourseAu,
+			registration,
+			courseAu
+		] = sessionReturn;
+		
+		expect(session).to.equal(2);
+		expect(regCourseAu).to.equal(13);
+		expect(registration).to.equal(26);
+		expect(courseAu).to.equal(4);
+		expect(txnRollback).to.be.false;
+
+		assert.equal(queryResult, sessionReturn)
+		expect(queryResult).to.equal(sessionReturn);
+
+		lfcStub.reset();
+		lfcStub.restore();
+	}),
+
+	it('catches and throws an error if the Session information was not retrieved successfully and rolls back transaction', async function() {
+		
+		abandonStub.callsFake(() => Promise.reject('cant retrieve').then(txnRollback = true));
+		await expect(Session.abandon(sessionId, tenantId, by, {db, lrsWreck})).to.be.rejectedWith(`cant retrieve`);
+
+		if(txnRollback == true){
+			function error () {
+				throw Error(`Boom error (internal)`);
+			}
+		}
+
+		expect(txnRollback).to.be.true;
+		expect(error).to.throw('Boom error (internal)');
+	})
+
+	it.skip('verifies queryResult was retrieved and uses it to assign session registration information', async function() {
+	
+		lfcStub.callsFake(() => Promise.resolve(txnReturn).then(queryResult = true));
+		expect(Session.loadForChange(txn, sessionId, tenantId));
+
+		if (queryResult == true) {
+
+			txnRollback = false;
+
+			var [session,
+					regCourseAu,
+					registration,
+					courseAu
+				] = txnReturn;
+
+			regCourseAuObject.courseAu = courseAu;
+		}
+
+		expect(session).to.equal(2);
+		expect(regCourseAu).to.equal(13);
+		expect(registration).to.equal(26);
+		expect(courseAu).to.equal(4);
+		expect(regCourseAuObject.courseAu).to.equal(4);
+		expect(txnRollback).to.be.false;
+	}),
+	
+	it('deterimines the Session is niether abandoned or terminated, and has it update and commit the transaction', async function()  {
+		
+		///This part of abandon function is all done via other functions, which will be tested separetly on their own.
+		///There is no more logic executed in this function, but wanted to leave this space to describe what the rest 
+		///of 'abandon does for any future questions or updates. 
+	})
+	
 })
+
