0d4318b9bffcd3cc466f827a31e9d5d4a272029a
Megan <megan.bohland@simisinc.com>
2022-04-18 20:45:59 -0400

    Finished some more functions.

--- player/test/registration.spec.js
+++ player/test/registration.spec.js
@@ -3,7 +3,7 @@ const sinon = require('sinon'); //base sinon
 const chai = require ("chai"); //base chai
 const chaiAsPromised = require("chai-as-promised");
 const Registration = require('../service/plugins/routes/lib/registration.js');//So Session is exported, can we get it's function here through this?
-const RegistrationFunctions = require('../service/plugins/routes/lib/registration.js');//So Session is exported, can we get it's function here through this?
+//const RegistrationFunctions = require('../service/plugins/routes/lib/registration.js');//So Session is exported, can we get it's function here through this?
 
 const { assert } = require('chai');
 const Wreck = require("@hapi/wreck");
@@ -19,7 +19,7 @@ describe.skip('Test of mapMoveOnChildren function', function() {
 
      ////map move on children is NOT counted as a function????
 	var moveOnChildrenSpy, getSessionStub;
-	var child=  sinon.spy(Registration, "");//stand in child object 
+	//var child=  sinon.spy(Registration, "");//stand in child object 
 	var db = {}; //stand in for database
 
 	beforeEach(() =>{
@@ -76,58 +76,61 @@ describe.skip('Test of mapMoveOnChildren function', function() {
 
 describe('Test of tryParseTemplate function', function() {
 
-	var tptSpy, getSessionStub;
-	var db = {}; //stand in for database
-     var statement, satisfiedStTemplate = "a satisfied template"
-	beforeEach(() =>{
-		//getSessionStub = sinon.stub(Session, 'getSession');
-		tptSpy = sinon.spy(RegistrationFunctions, 'tryParseTemplate');
+	var tptSpy, parseStub;
+     var statement; 
+     var satisfiedStTemplate ;
+
+     beforeEach(() =>{
+		parseStub = sinon.stub(JSON, 'parse')
+		tptSpy = sinon.spy(Registration, 'tryParseTemplate');
 
 	});
 
 	afterEach(() =>{
-		//getSessionStub.reset();
-		//getSessionStub.restore();
+		parseStub.reset();
+		parseStub.restore();
 
-		tptSpy.reset();
+          tptSpy.restore();
 	});
 
-	it('accepts a "child" parameter and maps to its corresponding information', function()  {
+	it('tries to parse a JSON statement template', async function()  {
 		
-          statement = RegistrationFunctions.tryParseTemplate(satisfiedStTemplate);
-          //getSessionStub.withArgs(sessionId, db).resolves(db)
+          parseStub.withArgs(satisfiedStTemplate).returns('a parsed template');
+          statement =  Registration.tryParseTemplate(satisfiedStTemplate);
+
+          Registration.tryParseTemplate.restore();
+          JSON.parse.restore();
 		
-		//test = await Session.load(sessionId, db);
-       //   Registration.mapMoveOnChildren(child);
-		
-        //  Registration.mapMoveOnChildren.restore();
-		//Session.getSession.restore();
+          expect(tptSpy.calledOnceWithExactly(satisfiedStTemplate)).to.be.true;
 
-		expect(tptSpy.calledOnceWithExactly(satisfiedStTemplate)).to.be.true;
+		expect(parseStub.calledOnceWithExactly(satisfiedStTemplate)).to.be.true;
 
-	//	expect(getSessionStub.calledOnceWithExactly(sessionId, db)).to.be.true;
-
-	//	expect(test).to.equal(db);
+		expect(statement).to.equal('a parsed template');
 	})
 
-	it.skip('throws an error if it cannot retrieve database informaton,', async function (){
+	it('throws an error if it cannot parse the template',  function (){
 		
+          parseStub.withArgs(satisfiedStTemplate).rejects();
+
 		try{
-			await Session.load(sessionId, db);
+			statement =  Registration.tryParseTemplate(satisfiedStTemplate);
 			assert.fail(error);//ensure promise was rejected, ie no false positive test
 		}
 	  	catch (ex) {
 			function error () {			
-			throw new Error(`Failed to select session: ${ex}`);
+			throw new Error(`Failed to parse statement template: ${ex}`);
 			}
 			
 			//error has to be wrapped and tested here, or it will throw and interrupt test execution
-			expect(error).to.throw(`Failed to select session: ${ex}`);
+			expect(error).to.throw(`Failed to parse statement template: ${ex}`);
 		}
 
-		Session.load.restore();
-		
-		expect(loadSpy.calledOnceWithExactly(sessionId, db)).to.be.true;
-	})
+		Registration.tryParseTemplate.restore();
+          JSON.parse.restore();
+
+		expect(tptSpy.calledOnceWithExactly(satisfiedStTemplate)).to.be.true;
+
+		expect(parseStub.calledOnceWithExactly(satisfiedStTemplate)).to.be.true;
+     })
 
 })
